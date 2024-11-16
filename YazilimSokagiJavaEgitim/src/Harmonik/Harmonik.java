1335d1338a3555412211a4fb7a149cf650b4924a
Adem Demir <93316402+AdemDemirFE@users.noreply.github.com>
2024-11-16 18:22:09 +0300

    Atm, EbobEkok, Harmonic, Ucgen

---
+++ YazilimSokagiJavaEgitim/src/Harmonik/Harmonik.java
@@ -0,0 +1,13 @@
+package Harmonik;
+
+public class Harmonik {
+    public static double harmonic(int n) {
+        if (n == 1) return 1.0;
+        else {
+            return (1.0 / n) + harmonic(n - 1);
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(harmonic(5));
+    }
+}
---
+++ YazilimSokagiJavaEgitim/src/Ucgen/Ucgen.java
@@ -0,0 +1,21 @@
+package Ucgen;
+
+import java.util.Scanner;
+
+public class Ucgen {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        // Üçgenin kenar uzunlukları
+        int a,b,c;
+        System.out.println("Üçgenin bütün kenarlarını sırasıyla giriniz:");
+        a = s.nextInt();
+        b = s.nextInt();
+        c = s.nextInt();
+
+        int u = (a + b + c) / 2;
+
+        double alan = Math.sqrt(u * (u-a) * (u-b) * (u-c));
+        System.out.println("Üçgenin alanı: " + alan);
+    }
+}
