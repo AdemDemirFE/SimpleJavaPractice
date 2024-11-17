646f457eb44bd921dc129133aae22eb4adade536
Adem Demir <93316402+AdemDemirFE@users.noreply.github.com>
2024-11-17 21:58:08 +0300

    kdv tutari hesaplayan program

--- YazilimSokagiJavaEgitim/src/KdvTutariHesaplama/KdvTutariHesaplama.java
+++ YazilimSokagiJavaEgitim/src/KdvTutariHesaplama/KdvTutariHesaplama.java
@@ -1,2 +1,14 @@
-package KdvTutariHesaplama;public class KdvTutariHesaplama {
+package KdvTutariHesaplama;
+
+import java.util.Scanner;
+
+public class KdvTutariHesaplama {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Ücret değerini giriniz: ");
+        double ucret = s.nextInt();
+        double kdv = ucret > 1000 ? 8 : 18;
+        double vergiliUcret = ucret + (ucret*kdv / 100);
+        System.out.print("Alacağınız ürünün KDV ile oluşan yeni fiyatı: " + vergiliUcret);
+    }
 }
